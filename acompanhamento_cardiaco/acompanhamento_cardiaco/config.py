e4acc3cfc180d20dc01dec0e922e19baaf3072a7
Renan Alves <renan.c.alves03@gmail.com>
2026-06-06 10:15:24 -0300

    config alterado com variáveis de ambiente corretas

--- acompanhamento_cardiaco/acompanhamento_cardiaco/config.py
+++ acompanhamento_cardiaco/acompanhamento_cardiaco/config.py
@@ -2,14 +2,14 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    SECRET_KEY: str
-    ALGORITHM: str = "HS256"
+    SECRET_KEY: str = 'chave-de-desenvolvimento-acompanhamento-cardiaco'
+    ALGORITHM: str = 'HS256'
 
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
 
     class Config:
-        env_file = ".env"
+        env_file = '.env'
 
 
 settings = Settings()
