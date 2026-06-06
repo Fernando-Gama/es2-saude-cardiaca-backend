66345b557d3533c76105d9b377fd31135bf47c71
Renan Alves <renan.c.alves03@gmail.com>
2026-06-06 10:22:32 -0300

    correção da rota do import auth_router

--- acompanhamento_cardiaco/acompanhamento_cardiaco/main.py
+++ acompanhamento_cardiaco/acompanhamento_cardiaco/main.py
@@ -1,14 +1,11 @@
 from fastapi import FastAPI
 
+from acompanhamento_cardiaco.auth.auth_router import router as auth_router
 from acompanhamento_cardiaco.database import Base, engine
 from acompanhamento_cardiaco.measurements.measurement_router import (
     router as measurements_router,
 )
 from acompanhamento_cardiaco.users.user_router import router as users_router
-from .database import Base, engine
-from .users.user_router import router as users_router
-from .auth.auth_router import router as auth_router
-
 
 Base.metadata.create_all(bind=engine)
 
@@ -16,7 +13,7 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(
     title="API de Acompanhamento de Saúde Cardíaca",
     version="1.0.0",
-    description=( 
+    description=(
         "API REST para apoiar pacientes com problemas cardíacos "
         "no monitoramento da saúde."
     ),
